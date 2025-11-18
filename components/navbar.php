e453b87375e502beb0a30f4b9ff149a6b799c307
rexia-intel-automation <mohamad@rexia.com.br>
2025-11-18 18:41:14 -0300

    Create navbar.php

---
+++ components/navbar.php
@@ -0,0 +1,7 @@
+<header class="navbar">
+    <div class="nav-left">
+        <a href="/"><img src="/assets/img/logo.svg" class="logo" alt="TIATFT"></a>
+    </div>
+
+    <button id="darkToggle" class="dark-toggle">🌓</button>
+</header>
