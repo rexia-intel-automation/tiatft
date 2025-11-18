5e0ab661cccd2efebadf89c8ca7f4d849b1762b2
rexia-intel-automation <mohamad@rexia.com.br>
2025-11-18 17:50:24 -0300

    Delete config directory

--- config/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-// /config/constants.php
-
-define('TIATFT_SITE_NAME', 'TIATFT - There Is a Template For That');
-define('TIATFT_TAGLINE', 'The Universal Template Marketplace');
-define('TIATFT_LAUNCH_TEXT', 'Public launch planned for March 2026.');
--- config/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-// /config/settings.php
-
-return [
-    'webhook_url'    => 'https://primary-production-55af6.up.railway.app/webhook/b52b4ed4-caa5-409b-aadc-e5f920642add',
-    'webhook_source' => 'tiaft_landing_waitlist',
-];
